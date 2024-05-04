9f3ab4be53ce991cd2d590489b8c44edcf3f24de
Vinaypunani <114213456+Vinaypunani@users.noreply.github.com>
2024-05-04 08:54:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-var cursor = document.querySelector("#cursor");
+var cursor1 = document.querySelector("#cursor01");
+var cursor02 = document.querySelector("#cursor02");
 var main = document.querySelector("#main");
 var box = document.querySelector("#box");
 
 main.addEventListener("mousemove",function(mp){
-    gsap.to(cursor,{
+    gsap.to(cursor1,{
         x:mp.x,
         y:mp.y,
         duration:0.5,
     })
 })
 
+main.addEventListener("mousemove",function(mp1){
+    gsap.to(cursor02,{
+        x:mp1.x,
+        y:mp1.y,
+        duration:1,
+    })
+})
+
 box.addEventListener("mouseenter",function(){
-    cursor.innerHTML="Visit Site"
-    gsap.to(cursor,{
-        scale:2,
+    cursor02.innerHTML="Visit Site"
+    gsap.to(cursor02,{
+        scale:1.5,
         duration: 1,
     })
 })
 
 box.addEventListener("mouseleave",function(){
-    cursor.innerHTML=""
-    gsap.to(cursor,{
+    cursor02.innerHTML=""
+    gsap.to(cursor02,{
         scale:1,
         duration: 1
     })
